fd192472f5fcf323c70697702e98cda5197e0437
Matt Burgess <matt@myclient.info>
2013-12-17 05:45:10 +0700

    new about us page

---
+++ app/views/about.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.scaffold')
+
+@section('main')
+
+
+<div id="content">
+
+	<div class="block-main">
+
+		<div class="container"> 
+
+			<div class="row">
+
+				<div class="span12">
+					
+					
+
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+@stop
